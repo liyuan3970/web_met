d984982e9c5a5dd4fc41841ea0b509568fd363e7
liyuan3970 <857956255@qq.com>
2022-02-15 16:09:23 +0000

    data

--- zdz/views.py
+++ zdz/views.py
@@ -19,34 +19,138 @@ from io import BytesIO
 import base64
 import json
 import pandas as pd
+import numpy as np
+
 def post_data(request):
-
-
-    
-
 
     #获取查询数据
     #print(request.POST)
-    start = request.POST.get('start','')
-    end = request.POST.get('end','')
-    crf = request.POST.get('csrfmiddlewaretoken','')
-    city = request.POST.get('city','')
+    start = request.POST.get('start', '')
+    end = request.POST.get('end', '')
+    crf = request.POST.get('csrfmiddlewaretoken', '')
+    city = request.POST.get('city', '')
     dicr = {
-        'start':start,
-        'end':end,
-        'crf':crf,
-        'city':city,
+        'start': start,
+        'end': end,
+        'crf': crf,
+        'city': city,
     }
     print(dicr)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ## 读取数据
     station_Mws = pd.read_csv("static/data/Mws2022.csv", keep_default_na=-9999)
     station_Aws = pd.read_csv("static/data/Aws2022.csv", keep_default_na=-9999)
-    station_all = pd.concat([station_Aws,station_Mws])
-    station_58665 = station_all[station_all['IIiii']==58665]
+    station_all = pd.concat([station_Aws, station_Mws])
+    station_58665 = station_all[station_all['IIiii'] == 58665]
     data_hj = station_58665['RR'].values.tolist()
-    print(data_hj[0:10])
-    
-    # 处理数据过程(分为降水模块、气温模块、能见度模块、风力模块)
+    grouped = station_all.groupby('IIiii')
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 数据1 单站总时间的统计数据 station_dot_all {}
+# 数据2 单站聚合时间数据数据 station_dot_comput {}
+# 数据3 市县的排序数据 station_county_comput {}
+# 数据4 统计站点分级数据 station_event_count {}
+
+
+    station_IIiii = station_all['IIiii']
+
+    station_name = station_IIiii.drop_duplicates().to_list()
+
+
+    station_dot_all = {}
+    for i in station_name:
+        station_dot_all[str(i)] = grouped.get_group(i)
+    station_dot_comput = {}
+    for i in station_name:
+        data = grouped.get_group(i)
+        data['VV'].replace(-9999, np.nan, inplace=True)
+        data['RR'].replace(-9999, np.nan, inplace=True)
+        data['Tn'].replace(-9999, np.nan, inplace=True)
+        dic = {}
+        dic['IIiii'] = data['IIiii'].iloc[0]
+        dic['StationName'] = data['StationName'].iloc[0]
+        dic['county'] = data['county'].iloc[0]
+        dic['lat'] = data['lat'].iloc[0]
+        dic['lon'] = data['lon'].iloc[0]
+        dic['Town'] = data['Town'].iloc[0]
+        dic['fFy'] = data['fFy'].max()
+        dic['dFy'] = data[data['fFy'] == data['fFy'].max()]['dFy'].iloc[0]
+        dic['T'] = data['T'].tolist()
+        dic['Tx'] = data['Tx'].max()/10.0
+        dic['Tn'] = data['Tn'].min()/10.0
+        dic['VV'] = data['VV'].min()
+        dic['RR'] = data['RR'].sum()
+        # 整合
+        station_dot_comput[str(i)] = dic
+
+    # 按照市县的排序数据
+    grouped_county = station_all.groupby('county')
+
+
+    station_county = {}
+    for i in grouped_county.size().index:
+    #     print(i)
+        station_county[i] = grouped_county.get_group(i)
+
+    station_county_comput = []
+    for i in grouped_county.size().index:
+        data = grouped_county.get_group(i)
+        data['VV'].replace(-9999, np.nan, inplace=True)
+        data['RR'].replace(-9999, np.nan, inplace=True)
+        data['Tn'].replace(-9999, np.nan, inplace=True)
+        dic = {}
+        dic['county'] = str(i)
+        dic['RR'] = data['RR'].mean()
+        dic['Tx'] = data['Tx'].max()
+        dic['Tn'] = data['Tn'].min()
+        dic['VV'] = data['VV'].min()
+        dic['fFy'] = data['fFy'].max()
+        dic['dFy'] = data[data['fFy'] == data['fFy'].max()]['dFy'].iloc[0]
+        print(dic)
+        station_county_comput.append(dic)
+
+    # 统计实际数据分级（降水、能见度）
+    station_event_count = {"RR": [], "VV": []}
+    count_vv_red = []
+    count_vv_orange = []
+    count_vv_yellow = []
+    count_vv_blue = []
+    vv_station_info = {"name": [], "lat": [], "lon": [], "value": []}
+    for i in station_name:
+        vv_data = station_dot_comput[str(i)]['VV']
+        if vv_data > 500 and vv_data < 1000:
+            count_vv_blue.append(1)
+            vv_station_info['name'].append(station_dot_comput[str(i)]['IIiii'])
+            vv_station_info['lat'].append(station_dot_comput[str(i)]['lat'])
+            vv_station_info['lon'].append(station_dot_comput[str(i)]['lon'])
+            vv_station_info['value'].append(station_dot_comput[str(i)]['VV'])
+        elif vv_data < 500 and vv_data > 250:
+            count_vv_yellow.append(1)
+            vv_station_info['name'].append(station_dot_comput[str(i)]['IIiii'])
+            vv_station_info['lat'].append(station_dot_comput[str(i)]['lat'])
+            vv_station_info['lon'].append(station_dot_comput[str(i)]['lon'])
+            vv_station_info['value'].append(station_dot_comput[str(i)]['VV'])
+        elif vv_data < 250 and vv_data > 50:
+            count_vv_orange.append(1)
+            vv_station_info['name'].append(station_dot_comput[str(i)]['IIiii'])
+            vv_station_info['lat'].append(station_dot_comput[str(i)]['lat'])
+            vv_station_info['lon'].append(station_dot_comput[str(i)]['lon'])
+            vv_station_info['value'].append(station_dot_comput[str(i)]['VV'])
+        elif vv_data < 50 and vv_data > 0:
+            count_vv_red.append(1)
+            vv_station_info['name'].append(station_dot_comput[str(i)]['IIiii'])
+            vv_station_info['lat'].append(station_dot_comput[str(i)]['lat'])
+            vv_station_info['lon'].append(station_dot_comput[str(i)]['lon'])
+            vv_station_info['value'].append(station_dot_comput[str(i)]['VV'])
+        elif vv_data > 1000:
+            vv_station_info['name'].append(station_dot_comput[str(i)]['IIiii'])
+            vv_station_info['lat'].append(station_dot_comput[str(i)]['lat'])
+            vv_station_info['lon'].append(station_dot_comput[str(i)]['lon'])
+            vv_station_info['value'].append(station_dot_comput[str(i)]['VV'])
+
+    station_event_count = {"RR": [], "VV": {"red": sum(count_vv_red), "blue": sum(count_vv_blue), "yellow": sum(count_vv_yellow), "orange": sum(count_vv_orange)}}
+
+# 处理数据过程(分为降水模块、气温模块、能见度模块、风力模块)
 
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -145,49 +249,82 @@ def post_data(request):
     # 指标站的时序图
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
     # 最低气温
-    tmp_min_County = [
-        {"name": '仙居县', "value": 100},
-        {"name": '椒江区', "value": 200},
-        {"name": '黄岩区', "value": 300},
-        {"name": '路桥区', "value": 400},
-        {"name": '三门县', "value": 500},
-        {"name": '天台县', "value": 600},
-        {"name": '温岭市', "value": 700},
-        {"name": '临海市', "value": 800},
-        {"name": '玉环市', "value": 900}
-    ]
+    nation_station = ['58660','58666','K8505','K8206','58665','58559','58655','K8271','58662','58653']
+    temp_scatter_list = ['58559','K8705','K8706','58652','K8903','58568','K8818','58662','K8821','58660','58653','K8609','K8505','58667','58664','K8413','58655','K8282','K8217','K8201','K8301','58665']
+    # 散点数据
+    tmp_min_scatter = []
+    tmp_max_scatter = []
+    for i in temp_scatter_list:
+        dic_temp_max = {"value":[],"url":"",}
+        dic_temp_max['value'].append(station_dot_comput[i]['lon'])
+        dic_temp_max['value'].append(station_dot_comput[i]['lat'])
+        dic_temp_max['value'].append(station_dot_comput[i]['Tx'])
+        dic_temp_max['url'] ="station/"+str(station_dot_comput[i]['IIiii'])
+        dic_temp_max['name'] = str(station_dot_comput[i]['StationName'])
+        dic_temp_min = {"value":[],"url":""}
+        dic_temp_min['value'].append(station_dot_comput[i]['lon'])
+        dic_temp_min['value'].append(station_dot_comput[i]['lat'])
+        dic_temp_min['value'].append(station_dot_comput[i]['Tn'])
+        dic_temp_min['url'] ="station/"+str(station_dot_comput[i]['IIiii'])
+        dic_temp_min['name'] = str(station_dot_comput[i]['StationName'])
+        tmp_max_scatter.append(dic_temp_max)
+        tmp_min_scatter.append(dic_temp_min)
+        #print(tmp_max_scatter)
+    # 柱状图数据
+    tmp_station_bar = []
+    tmp_station_bar.append(['product', '最高气温','最低气温'])
+    for i in nation_station:
+        tmp_station_bar.append([station_dot_comput[i]['StationName'], station_dot_comput[i]['Tx'],station_dot_comput[i]['Tn']])
+    
+    # 面数据
+    tmp_max_County = []
+    tmp_min_County = []
+    for i in station_county_comput:
+        tmp_max_County.append({"name":i['county'],"value":str(i['Tx'])})
+        tmp_min_County.append({"name":i['county'],"value":str(i['Tn'])})
+    # tmp_min_County = [
+    #     {"name": '仙居县', "value": 100},
+    #     {"name": '椒江区', "value": 200},
+    #     {"name": '黄岩区', "value": 300},
+    #     {"name": '路桥区', "value": 400},
+    #     {"name": '三门县', "value": 500},
+    #     {"name": '天台县', "value": 600},
+    #     {"name": '温岭市', "value": 700},
+    #     {"name": '临海市', "value": 800},
+    #     {"name": '玉环市', "value": 900}
+    # ]
     # 散点最低气温
-    tmp_min_scatter = [{ "value": [121.5, 28.5, 80], "url": "station/58667" }]
+    # tmp_min_scatter = [{ "value": [121.5, 28.5, 80], "url": "station/58667" ,"name":"58667"}]
 
     # 最低气温
-    tmp_max_County = [
-        {"name": '仙居县', "value": 100},
-        {"name": '椒江区', "value": 200},
-        {"name": '黄岩区', "value": 300},
-        {"name": '路桥区', "value": 400},
-        {"name": '三门县', "value": 500},
-        {"name": '天台县', "value": 600},
-        {"name": '温岭市', "value": 700},
-        {"name": '临海市', "value": 800},
-        {"name": '玉环市', "value": 900}
-    ]
+    # tmp_max_County = [
+    #     {"name": '仙居县', "value": 100},
+    #     {"name": '椒江区', "value": 200},
+    #     {"name": '黄岩区', "value": 300},
+    #     {"name": '路桥区', "value": 400},
+    #     {"name": '三门县', "value": 500},
+    #     {"name": '天台县', "value": 600},
+    #     {"name": '温岭市', "value": 700},
+    #     {"name": '临海市', "value": 800},
+    #     {"name": '玉环市', "value": 900}
+    # ]
     # 散点最低气温
-    tmp_max_scatter = [{ "value": [121.5, 28.5, 80], "url": "url_data" }]
+    # tmp_max_scatter = [{ "value": [121.5, 28.5, 80], "url": "url_data" }]
 
     # 指标站的时序图
-    tmp_station_bar = [
-        ['product', '气温时序'],
-        ['市局', 43.3],
-        ['天台', 83.1],
-        ['仙居', 86.4],
-        ['三门', 72.4],
-        ['临海', 72.4],
-        ['椒江', 72.4],
-        ['黄岩', 72.4],
-        ['路桥', 72.4],
-        ['温岭', 72.4],
-        ['玉环', 72.4],
-    ]
+    # tmp_station_bar = [
+    #     ['product', '最高气温','最低气温'],
+    #     ['市局', 43.3, 43.3],
+    #     ['天台', 83.1, 43.3],
+    #     ['仙居', 86.4, 43.3],
+    #     ['三门', 72.4, 43.3],
+    #     ['临海', 72.4, 43.3],
+    #     ['椒江', 72.4, 43.3],
+    #     ['黄岩', 72.4, 43.3],
+    #     ['路桥', 72.4, 43.3],
+    #     ['温岭', 72.4, 43.3],
+    #     ['玉环', 72.4, 43.3],
+    # ]
 
     context = {
         'img': imd,
@@ -198,7 +335,7 @@ def post_data(request):
         'RR_bar':RR_station_bar,
         'tn':json.dumps(tmp_min_County),
         'tn_scatter':tmp_min_scatter,
-        'tx':json.dumps(tmp_min_County),
+        'tx':json.dumps(tmp_max_County),
         'tx_scatter':tmp_min_scatter,
         'tmp_bar':tmp_station_bar,
         'testdata':json.dumps(data_hj),
