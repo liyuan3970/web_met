eef29341ac538d92d338f7ed8dc8d58eb5976c5e
liyuan3970 <857956255@qq.com>
2022-02-22 14:34:49 +0000

    data

--- zdz/views.py
+++ zdz/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from . import func
+from . import data_class
 # Create your views here.
 
 def kuaibao(request):
