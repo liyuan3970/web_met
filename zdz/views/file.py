6b8d3ae77dc03065143bdb0a5f8cd8711d733d3b
hao <qh095210@sina.com>
2022-11-16 21:41:08 +0800

    建议pdf浏览

--- zdz/views/file.py
+++ zdz/views/file.py
@@ -6,11 +6,9 @@ from weasyprint import HTML
 @api_view(["post"])
 def pdf(request):
     # 接收前台数据
-    pdf_blob = request.data["pdf_blob"]
+    document = request.data["document"]
 
-    pdf = HTML(string='''
-    <h1>2323143</h1>
-    <p>12312321<p>
-''')
-    pdf_file = pdf.write_pdf()
+    pdf_file = HTML(string=document)
+
+    pdf_file = pdf_file.write_pdf()
     return HttpResponse(pdf_file, content_type="application/pdf")
