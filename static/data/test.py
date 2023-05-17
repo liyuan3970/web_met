1b3ae5d00e23d30c2822df6b80024b0c8a1f225a
liyuan3970 <857956255@qq.com>
2023-05-17 14:27:23 +0000

    data

--- static/data/test.py
+++ static/data/test.py
@@ -1,3 +1,4 @@
+
 import pymysql
 import pymssql 
 import numpy as np
@@ -7,9 +8,7 @@ import redis
 import datetime as dtt
 from datetime import timezone
 class station_zdz:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+    def __init__(self):
         self.rs = redis.Redis(host='127.0.0.1', port=6379)
         self.conn = pymysql.connect(host="127.0.0.1",port=3306,user="root",passwd="051219",db="ZJSZDZDB")
     def time_today(self):
@@ -112,7 +111,7 @@ class station_zdz:
             daydelay = dtt.timedelta(days=-1)
             daystar = (today + daydelay).strftime('%Y-%m-%d %H:%M:%S')
             redis_df['tTime'] = pd.to_datetime(redis_df['tTime'])
-            # 测试
+            # 测试 基本数据
             time = '2019-08-09 19:30:00'
             daystar = '2019-08-08 19:30:00'
             # 测试
@@ -132,124 +131,109 @@ class station_zdz:
     def sql_now(self,decode_type,area):
         '''根据date_type向redis中获取数据'''
         return data
-    def decode_time(self,data,times,area,boundary):
+    def decode_time(self):
         date_type = "table_minutes"
-        times = 60*3
-        SHA_TZ = timezone(
-            dtt.timedelta(hours=8),
-            name='Asia/Shanghai',
-        )
-        utc_now = dtt.datetime.utcnow().replace(tzinfo=dtt.timezone.utc)
-        today = utc_now.astimezone(SHA_TZ)
-        offset = dtt.timedelta(minutes=-times)
-        timeindex = (today + offset).strftime('%Y-%m-%d %H:%M:%S')
-        timeindex = '2019-08-09 19:00:00'
-        data = pickle.loads(self.rs.get(date_type))
-        if boundary!="all":
-            lat0 =  boundary[0]
-            lat1 =  boundary[1]
-            lon0 =  boundary[2]
-            lon1 =  boundary[3]
-        else:
-            lat0 = 25
-            lat1 = 35
-            lon0 = 110
-            lon1 = 125
-        if area=="nation":
-            remain = data[(data['lat']>lat0) & (data['lat']<lat1)  &  (data['lon']<lon1) & (data['lon']>lon0) & (pd.isnull(data['Type']))&(data['ZoomLevel']<6)&(data['tTime']>timeindex)]
-        if area=="regin":
-            remain = data[(data['lat']>lat0) & (data['lat']<lat1)  &  (data['lon']<lon1) & (data['lon']>lon0) & (data['Type']=="区域站")&(data['tTime']>timeindex)]
-        elif area=="all":
-            remain = data[(data['lat']>lat0) & (data['lat']<lat1)  &  (data['lon']<lon1) & (data['lon']>lon0)&(data['tTime']>timeindex)]
-        elif area=="main":
-            remain = data[(data['lat']>lat0) & (data['lat']<lat1)  &  (data['lon']<lon1) & (data['lon']>lon0) & (data['ZoomLevel']<6)&(data['tTime']>timeindex)]
-        return remain
-    def decode_rain(self,data):
-        data = data[data['Ri']>0]
-        rain=data.groupby(by=['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['Ri'].sum().reset_index()
-        table_list = rain.to_json(orient='records',force_ascii=False)
-        return table_list
-    def decode_tmin(self,data):
-        data = data[data['T']!=-9999]
-        tmin=data.groupby(by=['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['T'].min().reset_index()
-        table_list = tmin.to_json(orient='records',force_ascii=False)
-        return table_list
-    def decode_tmax(self,data):
-        data = data[data['T']!=-9999]
-        tmax = data.groupby(by=['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['T'].max().reset_index()
-        table_list = tmax.to_json(orient='records',force_ascii=False)
-        return table_list
-    def decode_view(self,data):
-        data = data[data['V']>=0]
-        view = data.groupby(by=['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['V'].min().reset_index()
-        table_list = view.to_json(orient='records',force_ascii=False)
-        return table_list
-    def decode_wind(self,data):
-        data = data[data['fFy']!=-9999]
-        grouped_IIiii = remain.groupby('IIiii')
-        table_list = []
-        for i in grouped_IIiii.size().index:
-            single = grouped_IIiii.get_group(i)
-            wind = single_data['fFy'].max()
-            index =  single_data[single_data['fFy'] == single_data['fFy'].max()].index.tolist()[0]
-            deg = single_data['dFy'][index]
-            single_dir = {
-                "IIiii":str(single_data['IIiii'].iloc[0]),
-                "StationName":str(single_data['StationName'].iloc[0]),
-                "Province":str(single_data['Province'].iloc[0]),
-                "City":str(single_data['City'].iloc[0]),
-                "County":str(single_data['County'].iloc[0]),
-                "Town":str(single_data['Town'].iloc[0]),
-                "lat":str(single_data['lat'].iloc[0]),
-                "lon":str(single_data['lon'].iloc[0]),
-                "fFy":str(wind),
-                "dFy":str(deg)          
+        data = self.get_redis(date_type)['data']
+        times = [60*24,60*12,60*6,60*3]
+        tables = ['24hours','12hours','6hours','3hours']
+        # today = self.time_today()
+        # 测试时间数据
+        end = '2019-08-09 21:00:00' 
+        today = dtt.datetime.strptime(end,'%Y-%m-%d %H:%M:%S')
+        value_list = ['Ri','Tx','Tn','fFy','V']
+        for i, value in enumerate(times):
+            offset = dtt.timedelta(minutes=-value)         
+            timeindex = (today + offset).strftime('%Y-%m-%d %H:%M:%S')
+            remain = data[data['tTime']>timeindex]
+            table = tables[i]
+            table_data_list = []
+            for j in value_list:
+                value_df = j
+                df = self.return_data(remain,value_df)
+                table_data_list.append(df)  
+            data_redis = {
+                "data_class":tables[i],
+                "data":remain,
+                "value_list":value_list,
+                "table_data_list":table_data_list
             }
-            table_list.append(single_dir)
-        return table_list
-    def decode_data(self,decode_type,times,area,boundary,data_type):
-        '''根据前段参数处理数据
-        返回列表数据
-        decode_type--类型
-        data_type--请求的数据类型'''
-        date_type = "table_minutes"  
-        data = pickle.loads(self.rs.get(date_type))['data']
-        if decode_type=='now':
-            print("解析当前时刻的降水数据")
-            data = self.sql_now()
-        elif decode_type=='station':
-            print("解析单站的降水数据")
-        elif decode_type=='aera':
-            print("解析面上的降水数据")
-            timedata = self.decode_time(data,times,area,boundary)
-            if data_type == "rain":
-                output = self.decode_rain(timedata)
-            elif data_type == "wind":
-                output = self.decode_wind(timedata)
-            elif data_type == "tmax":
-                output = self.decode_tmax(timedata)
-            elif data_type == "tmin":
-                output = self.decode_tmin(timedata)
-            elif data_type == "view":
-                output = self.decode_view(timedata)
-        return output
-
+            # 将保留的数据重新存储到redis中
+            self.rs.set(tables[i], pickle.dumps(data_redis))  
+    def get_regin(self,boundary,table_type):
+        '''解码单站数据'''
+        lat0 = boundary[0]
+        lat1 = boundary[1]
+        lon0 = boundary[2]
+        lon1 = boundary[3]
+        boundary_data =  data[(data['lat']>lat0) & (data['lat']<lat1)  &  (data['lon']<lon1) & (data['lon']>lon0)]
+        if table_type=="nation":
+            remain = data[(pd.isnull(data['Type']))&(data['ZoomLevel']<6)]
+        if table_type=="regin":
+            remain = data[(data['Type']=="区域站")]
+        elif table_type=="all":
+            remain = data
+        elif table_type=="main":
+            remain = data[(data['ZoomLevel']<6)]
+        return remain
+    def return_data(self,remain,value):
+        '''返回数据的类型
+        boundary 边界
+        table redis的表
+        value 要素 Ri fFy Tx Tn V  
+        table_type  nation regin main all 
+        '''
+        if value=="Ri":
+            re = remain[remain['Ri']>0]
+            df = re.groupby(['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['Ri'].sum().reset_index()
+            df['value'] = df['Ri']
+            output = df.to_json(orient='records',force_ascii=False)
+        elif value=="fFy":
+            re = remain[remain['fFy']>0]
+            #output = re
+            grouped_IIiii = re.groupby(['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])
+            all_list = []
+            for i in grouped_IIiii.size().index:
+                single = grouped_IIiii.get_group(i)
+                value = single[ single['fFy']== single['fFy'].max()].head(1)
+                all_list.append(value)
+            df = pd.concat(all_list) 
+            df['value'] = df['fFy']
+            output = df.to_json(orient='records',force_ascii=False)         
+        elif value=="Tx":
+            re = remain[remain['T']!=-9999]
+            df = re.groupby(['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['T'].max().reset_index() 
+            df['value'] = df['T']
+            output = df.to_json(orient='records',force_ascii=False)
+        elif value=="Tn":
+            re = remain[remain['T']!=-9999]
+            df = re.groupby(['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['T'].min().reset_index()
+            df['value'] = df['T']
+            output = df.to_json(orient='records',force_ascii=False)
+        elif value=="V":
+            re = remain[remain['V']!=-9999]
+            df = re.groupby(['IIiii','StationName','Province','City','County','Town','Type','lat','lon'])['V'].min().reset_index() 
+            df['value'] = df['V']
+            output = df.to_json(orient='records',force_ascii=False)
+        return df
+        
+        
+            
 
 start = '2019-08-08 06:00:00'
 end = '2019-08-08 18:00:00'  
 date_type = 'table_minutes'
 station = '58653'
-worker = station_zdz(start,end)
+worker = station_zdz()
 dataframe = None
 decode_type = "aera"
 times = 60*3
-area="nation"
+area="main" # nation region all main
 boundary = "all"
-data_type = "rain"
+data_type = "wind"
 # worker.decode_dataframe(dataframe,decode_type)
 #data = worker.single_station(station)
-#worker.upload2_redis_Minutes()
+# worker.upload2_redis_Minutes()
 # data = worker.get_redis(date_type)
 # data['data']
-
-worker.decode_data(decode_type,times,area,boundary,data_type)
+worker.decode_time()
+#len(worker.decode_data(decode_type,times,area,boundary,data_type))
